eb83e7025e6f02b93062d20cf556583b8a73b66d
Chris <chrisdel@protonmail.com>
2021-04-21 20:37:03 -0400

    24/24 test working

--- Random_Code_Challenges/containsCloseNums.py
+++ Random_Code_Challenges/containsCloseNums.py
@@ -1,29 +1,20 @@
 def containsCloseNums(nums, k):
     ## we are checking the differences between one element to the next
     nums_dict = {}
-    # for x in range(0,len(nums)):
-    #     if nums[x] not in nums_dict:
-    #         nums_dict[nums[x]] = 0
-    #     else:
-    #         nums_dict[nums[x]] += 1
-    counter = 0
-    is_dup = False
-    result = False
-    for i in range(0, len(nums)):
-        ## when we move to the next index we reset the coutner
-        counter = 0
-        for j in range(i+1,len(nums)):
-            ## now we iterrate through the rest of the array to and if there is a dup
-            counter += 1
-            if nums[i] == nums[j]:
+    is_dup = False;
+    result = True;
+    for x in range(0,len(nums)):
+        if nums[x] not in nums_dict:
+            nums_dict[nums[x]] = x
+        else:
+            if x - nums_dict[nums[x]] > k:
+                result = False;
+            else:
                 is_dup = True;
-                if counter > k:
-                    break
-                else:
-                    result = True;
-                    break
-        
+                result = True;
+                break
+            nums_dict[nums[x]] = x
+            
     if is_dup == False:
-        return result;
-    else:
-        return result;
+        return False;
+    return result;
